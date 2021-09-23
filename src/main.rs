cd48d4fbed254609cd71ea1479205c041ac23c7b
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2021-09-22 23:55:32 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,211 @@
+mod poetry;
+
+use regex::Regex;
+use serenity::{builder::CreateMessage, Result};
+use std::collections::HashSet;
+use std::env;
+use wikipedia;
+
+#[macro_use]
+extern crate partial_application;
+
+use serenity::{
+    async_trait,
+    client::bridge::gateway::{GatewayIntents, ShardId, ShardManager},
+    framework::standard::{
+        buckets::{LimitedFor, RevertBucket},
+        help_commands,
+        macros::{check, command, group, help, hook},
+        Args, CommandGroup, CommandOptions, CommandResult, DispatchError, HelpOptions, Reason,
+        StandardFramework,
+    },
+    http::Http,
+    model::{
+        channel::{Channel, Message},
+        gateway::Ready,
+        id::UserId,
+        permissions::Permissions,
+    },
+};
+use serenity::{http, prelude::*};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[group]
+#[commands(poetry_url)]
+struct Poetry;
+
+#[group]
+#[commands(wiki)]
+struct Wiki;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+#[tokio::main]
+async fn main() {
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let http = Http::new_with_token(&token);
+
+    // We will fetch your bot's owners and id
+    let (owners, bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                owners.insert(team.owner_user_id);
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match http.get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access the bot id: {:?}", why),
+            }
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    let framework = StandardFramework::new()
+        .configure(|c| {
+            c.with_whitespace(true)
+                .on_mention(Some(bot_id))
+                .prefix("nano, ")
+                // In this case, if "," would be first, a message would never
+                // be delimited at ", ", forcing you to trim your arguments if you
+                // want to avoid whitespaces at the start of each.
+                .delimiters(vec![", ", ","])
+                // Sets the bot's owners. These will be used for commands that
+                // are owners only.
+                .owners(owners)
+        })
+        .group(&POETRY_GROUP)
+        .group(&WIKI_GROUP)
+        .group(&GENERAL_GROUP);
+
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client!");
+    // For this example to run properly, the "Presence Intent" and "Server Members Intent"
+    // options need to be enabled.
+    // These are needed so the `required_permissions` macro works on the commands that need to
+    // use it.
+    // You will need to enable these 2 options on the bot application, and possibly wait up to 5
+    // minutes.
+    // .intents(GatewayIntents::all())
+    // .await
+    // .expect("Err creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
+
+// #[tokio::main]
+// async fn main() {
+//     let framework = StandardFramework::new()
+//         .configure(|c| c.prefix("~"))
+//         .group(&GENERAL_GROUP);
+
+//     // Login with a bot token from the environment
+//     let token = env::var("DISCORD_TOKEN").expect("token");
+//     let mut client = Client::builder(token)
+//         .event_handler(Handler)
+//         .framework(framework)
+//         .await
+//         .expect("Error creating client");
+
+//     // start listening for events by starting a single shard
+//     if let Err(why) = client.start().await {
+//         println!("An error occurred while running the client: {:?}", why);
+//     }
+// }
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    let wiki = wikipedia::Wikipedia::<wikipedia::http::default::Client>::default();
+    let page = wiki.page_from_title("Riemann zeta function".to_owned());
+    let wiki_text = page.get_summary().unwrap();
+    // hacky way of avoiding huge newlines around math text: work on making this better in the
+    // future
+    let re1 = Regex::new(r"\n +").unwrap();
+    let re2 = Regex::new(r"(?P<first>\w+)\{.*\}").unwrap();
+    let intermediate = re1.replace_all(&wiki_text, "");
+    let processed = re2.replace_all(&intermediate, "$first");
+    let sentences: Vec<&str> = processed.split(". ").take(3).collect();
+    msg.reply(ctx, "Gimme a sec....").await?;
+    if let Err(why) = msg.reply(ctx, sentences.join(". ")).await {
+        dbg!(why);
+    }
+
+    Ok(())
+}
+
+#[command]
+async fn wiki(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let wiki = wikipedia::Wikipedia::<wikipedia::http::default::Client>::default();
+    let titles = wiki.search(args.rest());
+    let page = wiki.page_from_title(titles.unwrap().iter().next().unwrap().to_string());
+    let wiki_text = page.get_summary().unwrap();
+    // hacky way of avoiding huge newlines around math text: work on making this better in the
+    // future
+    let re1 = Regex::new(r"\n +").unwrap();
+    let re2 = Regex::new(r"(?P<first>\w+)\{.*\}").unwrap();
+    let intermediate = re1.replace_all(&wiki_text, "");
+    let processed = re2.replace_all(&intermediate, "$first");
+    let sentences: Vec<&str> = processed.split(". ").take(3).collect();
+    if let Err(why) = msg.reply(ctx, sentences.join(". ")).await {
+        dbg!(why);
+    }
+
+    Ok(())
+}
+
+fn send_poem<'a, 'b>(
+    poem_opt: Option<poetry::Poem>,
+    m: &'a mut CreateMessage<'b>,
+) -> &'a mut CreateMessage<'b> {
+    match poem_opt {
+        None => m.content("Couldn't find poem!"),
+        Some(poem) => m.embed(|e| {
+            e.title(&poem.title);
+            let mut desc = "By ".to_string();
+            desc.push_str(&poem.poet);
+            e.description(&desc);
+            e.field("Poem", &poem.poem, false);
+            e.url(&poem.url)
+        }),
+    };
+
+    m
+}
+
+#[command]
+async fn poetry_url(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let url = args.rest();
+    let poem_opt = poetry::get_poem(url.to_string()).await;
+    let res = msg
+        .channel_id
+        .send_message(&ctx.http, partial!(send_poem => poem_opt, _))
+        .await;
+
+    if let Err(why) = res {
+        println!("Error sending message: {:?}", why);
+    }
+
+    Ok(())
+}
---
+++ src/poetry.rs
@@ -0,0 +1,61 @@
+//! Program to get poetry from the Web.
+use std::error::Error;
+
+use reqwest;
+use scraper;
+
+/// A poem with poet, link to site, and text.
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub(crate) struct Poem {
+    /// The poet's name.
+    pub poet: String,
+    /// The poem's text.
+    pub poem: String,
+    /// The poem's title.
+    pub title: String,
+    /// The URL to the poem.
+    pub url: String,
+}
+
+/// Given a URL to a poem, returns the full Poem object.
+pub(crate) async fn get_poem(url: String) -> Option<Poem> {
+    let content = reqwest::get(&url).await;
+    match content.ok() {
+        None => None,
+        Some(req) => {
+            let req_text = req.text().await;
+            match req_text.ok() {
+                None => None,
+                Some(body) => parse_poem(url, body),
+            }
+        }
+    }
+}
+
+/// Parses a poem given the URL and the HTML of the URL.
+fn parse_poem(url: String, content: String) -> Option<Poem> {
+    let html = scraper::Html::parse_fragment(&content);
+    let div_poem = scraper::Selector::parse("div.o-poem").unwrap();
+    let span_poet = scraper::Selector::parse("span.c-txt_attribution a").unwrap();
+    let h1_title = scraper::Selector::parse("h1").unwrap();
+    match html
+        .select(&div_poem)
+        .next()
+        .zip(html.select(&span_poet).next())
+        .zip(html.select(&h1_title).next())
+    {
+        None => None,
+        Some(((div, poet_span), title_h1)) => {
+            let text: Vec<_> = div.text().collect();
+            let poem = text.join("\n");
+            let poet = String::from(poet_span.text().next().unwrap_or("[Author not found]"));
+            let title = String::from(title_h1.text().next().unwrap_or("[Title not found]"));
+            Some(Poem {
+                poet,
+                poem,
+                title,
+                url,
+            })
+        }
+    }
+}
