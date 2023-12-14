8b10761d60806c6637b7daf51c4df785ff376705
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2023-12-14 00:00:30 -0500

    Modified say to be mysterious!

--- src/say.rs
+++ src/say.rs
@@ -1,5 +1,7 @@
 //! Command to echo given input text.
 
+use poise::serenity_prelude::CreateMessage;
+use serenity::http::CacheHttp;
 use serenity::utils::{content_safe, ContentSafeOptions};
 
 use crate::utils::{Context, Error};
@@ -7,7 +9,14 @@ use crate::utils::{Context, Error};
 /// Says whatever is given to say.
 ///
 /// Will remove mentions and other potential permissions issues.
-#[poise::command(slash_command)]
+#[poise::command(
+    slash_command,
+    prefix_command,
+    track_edits,
+    invoke_on_edit,
+    reuse_response,
+    track_deletion
+)]
 pub(crate) async fn say(
     ctx: Context<'_>,
     #[description = "The words to say"] message: String,
@@ -17,6 +26,11 @@ pub(crate) async fn say(
         None => ContentSafeOptions::default(),
     };
 
-    ctx.say(content_safe(ctx, message, &opts, &[])).await?;
+    ctx.channel_id()
+        .send_message(&ctx.http(), |m| {
+            m.content(content_safe(ctx, message, &opts, &[]))
+        })
+        .await?;
+
     Ok(())
 }
